93d4e0ff931a989ef77cd26dacae08a6eb77fdda
Eric Terlop <ETerlop@student.fullsail.edu>
2021-12-08 10:09:59 -0500

    Added form ejs and started adding validation for each input

--- app.js
+++ app.js
@@ -68,6 +68,72 @@ router.post("/login", (req, res) => {
     })
 })
 
+router.post("/register", (req, res) => {
+    console.log(req.body);
+    let errors = [];
+    // Validate the email not blank.
+    if(req.body.fname.trim() === "") {
+        errors.push("First name cannot be blank");
+    }
+
+    if(req.body.lname.trim() === "") {
+        errors.push("Last name cannot be blank");
+    }
+
+    if(req.body.address.trim() === "") {
+        errors.push("Address cannot be blank");
+    }
+
+    if(req.body.city.trim() === "") {
+        errors.push("City cannot be blank");
+    }
+
+    if(req.body.state.trim() === "") {
+        errors.push("State cannot be blank");
+    }
+
+    if(req.body.zip.trim() === "") {
+        errors.push("Zip cannot be blank");
+    }
+
+    if(req.body.age === "select") {
+        errors.push("Please select an age");
+    }
+
+    if(req.body.genders === undefined) {
+        errors.push("Please select a gender");
+    }
+
+    if(req.body.consent === undefined) {
+        errors.push("Please select the consent checkbox");
+    }
+
+    // if(req.body.consent.checked === false) {
+    //     errors.push("Please select the consent checkbox");
+    // }
+
+
+    //
+    // // Validate the password not blank.
+    // if(req.body.password.trim() === "") {
+    //     errors.push("Password cannot be blank. test");
+    // }
+    //
+    // // Validate the email incorrect format.
+    // if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+$/.test(req.body.email)) {
+    //     errors.push("Invalid Email address. test");
+    // }
+    //
+    // // Validate the password incorrect format.
+    // if(!/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*.])[\w!@#&%^&*.]{8,}$/.test(req.body.password)) {
+    //     errors.push("Invalid password format");
+    // }
+    res.render("register", {
+        pagename: "register",
+        regErr: errors,
+    })
+})
+
 
 
 
